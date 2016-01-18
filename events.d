b6a078b38a7153ba7167839d664e8fd523f9e7e3
Arun Thomas <arun.thomas@baesystems.com>
2016-01-18 11:35:10 -0500

    events.d: Add network probes

--- events.d
+++ events.d
@@ -41,3 +41,56 @@ syscall::fork*:entry
 {
 	printf("fork: %s %d", execname, pid);
 }
+
+
+ip:::send
+/pid != $pid/
+{
+	printf(" %3d %10d %15s -> %15s %8s %6d\n", cpu, timestamp,
+	    args[2]->ip_saddr, args[2]->ip_daddr, args[3]->if_name,
+	    args[2]->ip_plength);
+}
+
+ip:::receive
+/pid != $pid/
+{
+	printf(" %3d %10d %15s <- %15s %8s %6d\n", cpu, timestamp,
+	    args[2]->ip_daddr, args[2]->ip_saddr, args[3]->if_name,
+	    args[2]->ip_plength);
+}
+
+tcp:::send
+/pid != $pid/
+
+{
+	this->length = args[2]->ip_plength - args[4]->tcp_offset;
+	printf("%-3d %15s:%-5d  ->  %15s:%-5d %6d (", cpu,
+	    args[2]->ip_saddr, args[4]->tcp_sport,
+	    args[2]->ip_daddr, args[4]->tcp_dport, this->length);
+}
+
+tcp:::receive
+/pid != $pid/
+
+{
+	this->length = args[2]->ip_plength - args[4]->tcp_offset;
+	printf("%-3d %15s:%-5d  <-  %15s:%-5d %6d (", cpu,
+	    args[2]->ip_daddr, args[4]->tcp_dport,
+	    args[2]->ip_saddr, args[4]->tcp_sport, this->length);
+}
+
+tcp:::send,
+tcp:::receive
+/pid != $pid/
+{
+	printf("%s", args[4]->tcp_flags & TH_FIN ? "FIN|" : "");
+	printf("%s", args[4]->tcp_flags & TH_SYN ? "SYN|" : "");
+	printf("%s", args[4]->tcp_flags & TH_RST ? "RST|" : "");
+	printf("%s", args[4]->tcp_flags & TH_PUSH ? "PUSH|" : "");
+	printf("%s", args[4]->tcp_flags & TH_ACK ? "ACK|" : "");
+	printf("%s", args[4]->tcp_flags & TH_URG ? "URG|" : "");
+	printf("%s", args[4]->tcp_flags & TH_ECE ? "ECE|" : "");
+	printf("%s", args[4]->tcp_flags & TH_CWR ? "CWR|" : "");
+	printf("%s", args[4]->tcp_flags == 0 ? "null " : "");
+	printf("\b)\n");
+}
