d4fb091e7fbbeef23b2a80be65848a5981ed5b97
Arun Thomas <arun.thomas@baesystems.com>
2016-02-09 03:52:18 -0500

    events.d: Use JSON trace format and add new network events
    * Also add new attributes (e.g., uid, pid, ppid) for events
    * Network events are based on soconnect.d/soaccept.d
    * Network events are a WIP; more debugging required
    * TODO: Still need fds support for read/write events

--- events.d
+++ events.d
@@ -5,92 +5,138 @@
 #pragma D option dynvarsize=16m
 #pragma D option bufsize=8m
 
-syscall::open*:entry
+/* If AF_INET and AF_INET6 are "Unknown" to DTrace, replace with numbers: */
+inline int af_inet = 2 /*AF_INET*/;
+inline int af_inet6 = 28 /*AF_INET6*/;
+
+syscall::open:entry
 /pid != $pid/
 {
-	printf("At %Y %s file opened by PID %d UID %d using %s\n",
-	    walltimestamp, copyinstr(arg0), pid, uid, execname);
+	printf("{\"event\": \"%s\", \"time\": %d, \"pid\": %d, \"uid\": %d, \"exec\": \"%s\", \"path\": \"%s\"}\n",
+	    probefunc, walltimestamp, pid, uid, execname, copyinstr(arg0));
 }
 
-/* Temporarily disabled since FreeBSD does not support fi_pathname
-
-syscall::read*:entry,syscall::write*:entry
+syscall::openat:entry
 /pid != $pid/
 {
-	printf("At %Y %s file %s by %s\n",
-	    walltimestamp, fds[arg0].fi_pathname, probefunc, execname);
+	printf("{\"event\": \"%s\", \"time\": %d, \"pid\": %d, \"uid\": %d, \"exec\": \"%s\", \"path\": \"%s\"}\n",
+	    probefunc, walltimestamp, pid, uid, execname, copyinstr(arg1));
 }
 
-*/
-
-proc:::exec-success
-/pid != $pid/
+/* TODO: Add support for fds[arg0].fi_pathname */
+syscall::read:entry,syscall::write:entry,
+syscall::pread:entry,syscall::pwrite:entry,
+syscall::readv:entry,syscall::writev:entry,
+syscall::pread:entry,syscall::pwrite:entry
+/pid != $pid && execname != "sshd" && execname != "tmux"/
 {
-	trace(curpsinfo->pr_psargs);
+	printf("{\"event\": \"%s\", \"time\": %d, \"pid\": %d, \"uid\": %d, \"exec\": \"%s\", \"path\": \"TODO\"}\n",
+	    probefunc, walltimestamp, pid, uid, execname);
 }
 
-
-syscall::exec*:return
+syscall::execve:entry
 /pid != $pid/
 {
-	printf("exec: %Y %s\n", walltimestamp, curpsinfo->pr_psargs);
-}
-
-syscall::fork*:entry
-/pid != $pid/
-{
-	printf("fork: %s %d", execname, pid);
+	printf("{\"event\": \"%s\", \"time\": %d, \"pid\": %d, \"ppid\": %d, \"uid\": %d, \"exec\": \"%s\"}\n",
+	    probefunc, walltimestamp, pid, curpsinfo->pr_ppid, uid, copyinstr(arg0));
 }
 
 
-ip:::send
+syscall::fork:entry,syscall::rfork:entry,syscall::vfork:entry
 /pid != $pid/
 {
-	printf(" %3d %10d %15s -> %15s %8s %6d\n", cpu, timestamp,
-	    args[2]->ip_saddr, args[2]->ip_daddr, args[3]->if_name,
-	    args[2]->ip_plength);
+	printf("{\"event\": \"%s\", \"time\": %d, \"pid\": %d, \"uid\": %d, \"exec\": \"%s\"}\n",
+	    probefunc, walltimestamp, pid, uid, execname);
+
 }
 
-ip:::receive
+syscall::exit:entry
 /pid != $pid/
 {
-	printf(" %3d %10d %15s <- %15s %8s %6d\n", cpu, timestamp,
-	    args[2]->ip_daddr, args[2]->ip_saddr, args[3]->if_name,
-	    args[2]->ip_plength);
+	printf("{\"event\": \"%s\", \"time\": %d, \"pid\": %d, \"uid\": %d, \"exec\": \"%s\"}\n",
+	    probefunc, walltimestamp, pid, uid, execname);
 }
 
-tcp:::send
-/pid != $pid/
-
+syscall::connect*:entry
 {
-	this->length = args[2]->ip_plength - args[4]->tcp_offset;
-	printf("%-3d %15s:%-5d  ->  %15s:%-5d %6d (", cpu,
-	    args[2]->ip_saddr, args[4]->tcp_sport,
-	    args[2]->ip_daddr, args[4]->tcp_dport, this->length);
+	/* assume this is sockaddr_in until we can examine family */
+	this->s = (struct sockaddr_in *)copyin(arg1, sizeof (struct sockaddr));
+	this->f = this->s->sin_family;
 }
 
-tcp:::receive
-/pid != $pid/
-
+syscall::connect*:entry
+/this->f == af_inet/
 {
-	this->length = args[2]->ip_plength - args[4]->tcp_offset;
-	printf("%-3d %15s:%-5d  <-  %15s:%-5d %6d (", cpu,
-	    args[2]->ip_daddr, args[4]->tcp_dport,
-	    args[2]->ip_saddr, args[4]->tcp_sport, this->length);
+	self->family = this->f;
+	self->port = ntohs(this->s->sin_port);
+	self->address = inet_ntop(self->family, (void *)&this->s->sin_addr);
+	self->start = timestamp;
 }
 
-tcp:::send,
-tcp:::receive
-/pid != $pid/
+syscall::connect*:entry
+/this->f == af_inet6/
 {
-	printf("%s", args[4]->tcp_flags & TH_FIN ? "FIN|" : "");
-	printf("%s", args[4]->tcp_flags & TH_SYN ? "SYN|" : "");
-	printf("%s", args[4]->tcp_flags & TH_RST ? "RST|" : "");
-	printf("%s", args[4]->tcp_flags & TH_PUSH ? "PUSH|" : "");
-	printf("%s", args[4]->tcp_flags & TH_ACK ? "ACK|" : "");
-	printf("%s", args[4]->tcp_flags & TH_URG ? "URG|" : "");
-	printf("%s", args[4]->tcp_flags & TH_ECE ? "ECE|" : "");
-	printf("%s", args[4]->tcp_flags & TH_CWR ? "CWR|" : "");
-	printf("%s", args[4]->tcp_flags == 0 ? "null " : "");
-	printf("\b)\n");
+	/* refetch for sockaddr_in6 */
+	this->s6 = (struct sockaddr_in6 *)copyin(arg1,
+	    sizeof (struct sockaddr_in6));
+	self->family = this->f;
+	self->port = ntohs(this->s6->sin6_port);
+	self->address = inet_ntoa6((in6_addr_t *)&this->s6->sin6_addr);
+	self->start = timestamp;
+}
+
+syscall::connect*:return
+/self->start/
+{
+	this->delta = (timestamp - self->start) / 1000;
+	printf("{\"event\": \"%s\", \"time\": %d, \"pid\": %d, \"uid\": %d, \"exec\": \"%s\", \"family\": %d, \"address\": \"%s\", \"port\": %d, \"err\": %d}\n",
+	    probefunc, walltimestamp, pid, uid, execname, self->family, self->address, self->port, errno);
+	self->family = 0;
+	self->address = 0;
+	self->port = 0;
+	self->start = 0;
+}
+
+syscall::accept*:entry
+{
+	self->sa = arg1;
+	self->start = timestamp;
+}
+
+syscall::accept*:return
+/self->sa/
+{
+	this->delta = (timestamp - self->start) / 1000;
+	/* assume this is sockaddr_in until we can examine family */
+	this->s = (struct sockaddr_in *)copyin(self->sa,
+	    sizeof (struct sockaddr_in));
+	this->f = this->s->sin_family;
+}
+
+syscall::accept*:return
+/this->f == af_inet/
+{
+	this->port = ntohs(this->s->sin_port);
+	this->address = inet_ntoa((in_addr_t *)&this->s->sin_addr);
+	printf("{\"event\": \"%s\", \"time\": %d, \"pid\": %d, \"uid\": %d, \"exec\": \"%s\", \"family\": %d, \"address\": \"%s\", \"port\": %d, \"err\": %d}\n",
+	    probefunc, walltimestamp, pid, uid, execname, this->f, this->address, this->port, errno);
+
+}
+
+syscall::accept*:return
+/this->f == af_inet6/
+{
+	/* refetch for sockaddr_in6 */
+	this->s6 = (struct sockaddr_in6 *)copyin(self->sa,
+	    sizeof (struct sockaddr_in6));
+	this->port = ntohs(this->s6->sin6_port);
+	this->address = inet_ntoa6((in6_addr_t *)&this->s6->sin6_addr);
+	printf("{\"event\": \"%s\", \"time\": %d, \"pid\": %d, \"uid\": %d, \"exec\": \"%s\", \"family\": %d, \"address\": \"%s\", \"port\": %d, \"err\": %d}\n",
+	    probefunc, walltimestamp, pid, uid, execname, this->f, this->address, this->port, errno);
+}
+
+syscall::accept*:return
+/self->start/
+{
+	self->sa = 0; self->start = 0;
 }
