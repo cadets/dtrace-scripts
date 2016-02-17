26b4680e90154f43a25f668cf647b7a1bd19c3d4
Arun Thomas <arun.thomas@baesystems.com>
2016-02-17 15:32:07 -0500

    gitserver.d: simplify and use new format

--- gitserver.d
+++ gitserver.d
@@ -11,47 +11,15 @@ END {
   printf("]\n");
 }
 
-/* Used for git pull, fetch, and git clone 
- * So far, git pull and git fetch appear identically.
- * git clone will also show this, but there are additional git calls that
- * indicate it's actually a clone.
+/* Used for Git and SSH
+ * - Captures git command-line arguments
+ * - Some of the sshd's show the connecting user - on git connections, shows up
+ *   as username@notty in the curpsinfo->pr_psargs
  */
 syscall::exec*:
-/execname == "git-upload-pack"/
+/execname == "git-receive-pack" || execname == "git-upload-pack" || execname == "git" || execname == "sshd"/
 {
-	printf("%s {\"event\": \"%s:%s\", \"time\": %d, \"pid\": %d, \"tid\": %d, \"uid\": %d, \"exec\": \"%s\", \"meaning\": \"%s\"}\n",
-	    comma, "git", "upload-pack", walltimestamp, pid, tid, uid, execname, "pull, fetch, or clone");
+	printf("%s {\"event\": \"%s:%s:%s:%s\", \"time\": %d, \"pid\": %d, \"tid\": %d, \"uid\": %d, \"exec\": \"%s\", \"args\": \"%s\"}\n",
+	    comma, probeprov, probemod, probefunc, probename, walltimestamp, pid, tid, uid, execname, stringof(curpsinfo->pr_psargs));
 	comma=",";
 }
-
-/* Used for git push */
-syscall::exec*:
-/execname == "git-receive-pack"/
-{
-	printf("%s {\"event\": \"%s:%s\", \"time\": %d, \"pid\": %d, \"tid\": %d, \"uid\": %d, \"exec\": \"%s\", \"meaning\": \"%s\"}\n",
-	    comma, "git", "receive-pack", walltimestamp, pid, tid, uid, execname, "push");
-	comma=",";
-}
-
-/* Some of the sshd's show the connecting user - on git connections, shows up
- * as username@notty in the curpsinfo->pr_psargs
- */
-syscall::exec*:
-/execname == "sshd"/
-{
-	printf("%s {\"event\": \"%s:%s\", \"time\": %d, \"pid\": %d, \"tid\": %d, \"uid\": %d, \"exec\": \"%s\", \"user\": \"%s\"}\n",
-	    comma, "ssh", "incoming", walltimestamp, pid, tid, uid, execname, stringof(curpsinfo->pr_psargs));
-	comma=",";
-}
-
-/* What does the git executable have to say
- */
-syscall::exec*:
-/execname == "git"/
-{
-	printf("%s {\"event\": \"%s:%s\", \"time\": %d, \"pid\": %d, \"tid\": %d, \"uid\": %d, \"exec\": \"%s\", \"details\": \"%s\"}\n",
-	    comma, "git", "command", walltimestamp, pid, tid, uid, execname, stringof(curpsinfo->pr_psargs));
-	comma=",";
-}
-
-
