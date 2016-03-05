6e95baa83c7c9f21346a7b52fd67e4e230e26bac
Arun Thomas <arun.thomas@baesystems.com>
2016-03-04 23:30:21 -0500

    Simplify postgres script

--- postgres.d
+++ postgres.d
@@ -5,10 +5,6 @@
 #pragma D option dynvarsize=16m
 #pragma D option bufsize=8m
 
-/* If AF_INET and AF_INET6 are "Unknown" to DTrace, replace with numbers: */
-inline int af_inet = 2 /*AF_INET*/;
-inline int af_inet6 = 28 /*AF_INET6*/;
-
 BEGIN {
 	printf("[\n");
 	comma=" ";
@@ -18,16 +14,7 @@ END {
   printf("]\n");
 }
 
-postgresql$target:::transaction*
-{
-    /* probeprov:probemod:probefunc:probname */
-	printf("%s {\"event\": \"%s:%s:%s:%s\", \"time\": %d, \"pid\": %d, \"tid\": %d, \"uid\": %d, \"exec\": \"%s\", \"id\": \"%s\"}\n",
-	    comma, probeprov, probemod, probefunc, probename, walltimestamp, pid, tid, uid, execname, copyinstr(arg0));
-	comma=",";
-}
-
-postgresql$target:::query-start,
-postgresql$target:::query-done
+postgresql*:::query-start
 {
 	printf("%s {\"event\": \"%s:%s:%s:%s\", \"time\": %d, \"pid\": %d, \"tid\": %d, \"uid\": %d, \"exec\": \"%s\", \"query\": \"%s\"}\n",
 	    comma, probeprov, probemod, probefunc, probename, walltimestamp, pid, tid, uid, execname, copyinstr(arg0));
