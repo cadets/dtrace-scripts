d9740702ab22d5fc5b5bee92b15cea738c858a7a
Amanda Strnad <amanda.strnad@baesystems.com>
2016-04-29 10:21:09 -0400

    Graphs: add mmap to data tracking, add graph showing execs

--- generate_graphs.sh
+++ generate_graphs.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
 
-# 1st arguement is a CADETS trace file - will create a png 
+# 1st arguement is a CADETS trace file - will create a png
 # requires graphviz to be installed
 
 file_name=$(basename -s'.json' $1)
 
 {
-echo "strict digraph $file_name {"; 
+echo "strict digraph $file_name {";
 echo "rankdir=\"LR\";"
-sed -n -E 's/.*"path": "([ [:alnum:]\\\/_.-]*)".*/"f_\1" [label="\1"];/p' $1 | sort | uniq ; 
+sed -n -E 's/.*"path": "([ [:alnum:]\\\/_.-]*)".*/"f_\1" [label="\1"];/p' $1 | sort | uniq ;
 grep 'write' $1 | sed -n -E 's/.*"event": "[[:alnum:]_-]*:[[:alnum:]_-]*:(.?write[[:alnum:]_-]*):".*"pid": ([[:digit:]]*).*"exec": "([[:alnum:]_-]*)".*"path": "([ [:alnum:]\\\/_.-]*)".*/    p_\2 [label="pid \2", shape="diamond"];\
-    "p_\2_\3" [label="\3\
-p\2", shape="box"];\
+    "p_\2_\3" [label="{{\3 | p\2}}", shape="record"];\
     "p_\2_\3" -> "f_\4" [label="write", color="saddlebrown"];/p'
 grep 'read' $1 | sed -n -E 's/.*"event": "[[:alnum:]_-]*:[[:alnum:]_-]*:(.?read[[:alnum:]_-]*):".*"pid": ([[:digit:]]*).*"exec": "([[:alnum:]_-]*)".*"path": "([ [:alnum:]\\\/_.-]*)".*/    p_\2 [label="pid \2", shape="diamond"];\
-    "p_\2_\3" [label="\3\
-p\2", shape="box"];\
+    "p_\2_\3" [label="{{\3 | p\2}}", shape="record"];\
     "f_\4" -> "p_\2_\3" [label="read", color="black"];/p'
 grep 'mmap' $1 | sed -n -E 's/.*"event": "[[:alnum:]_-]*:[[:alnum:]_-]*:(.?mmap[[:alnum:]_-]*):".*"pid": ([[:digit:]]*).*"exec": "([[:alnum:]_-]*)".*"path": "([ [:alnum:]\\\/_.-]*)".*/    p_\2 [label="pid \2", shape="diamond"];\
-    "p_\2_\3" [label="\3\
-p\2", shape="box"];\
+    "p_\2_\3" [label="{{\3 | p\2}}", shape="record"];\
     "f_\4" -> "p_\2_\3" [label="mmap", color="black"];/p'
-echo "}" 
+echo "}"
 } > ${file_name}_data_flow.dot
 
 # make a png, removing orphaned nodes
@@ -31,16 +28,43 @@ gvpr -c 'N[$.degree==0]{delete($G,$);}' ${file_name}_data_flow.dot | dot -Tpng -
 # separates by pid and executable - display pid as a node
 
 {
-echo "strict digraph $file_name {"; 
+echo "strict digraph $file_name {";
 echo "rankdir=\"LR\";"
-sed -n -E 's/.*"path": "([ [:alnum:]\\\/_.-]*)".*/"f_\1" [label="\1"];/p' $1 | sort | uniq ; 
+sed -n -E 's/.*"path": "([ [:alnum:]\\\/_.-]*)".*/"f_\1" [label="\1"];/p' $1 | sort | uniq ;
 sed -n -E 's/.*"event": "[[:alnum:]_-]*:[[:alnum:]_-]*:([[:alnum:]_-]*):".*"pid": ([[:digit:]]*).*"exec": "([[:alnum:]_-]*)".*"path": "([ [:alnum:]\\\/_.-]*)".*/    p_\2 [label="pid \2", shape="diamond"];\
     "p_\2_\3" [label="\3", shape="box"];\
      p_\2 -> "p_\2_\3" [dir="none", minlen=2];\
-    "p_\2_\3" -> "f_\4" [label="\1"];/p' $1 
-echo "}" 
+    "p_\2_\3" -> "f_\4" [label="\1"];/p' $1
+grep 'new_exec' $1 | sed -n -E 's/.*"event": "[[:alnum:]_-]*:[[:alnum:]_-]*:(.?exec[[:alnum:]_-]*):".*"pid": ([[:digit:]]*).*"uid": ([[:digit:]]*).*"exec": "([[:alnum:]_-]*)".*"new_exec": "([ [:alnum:]_.-]*\/)*([ [:alnum:]_.-]*)".*/    p_\2 [label="pid \2", shape="diamond"];\
+    "e_\2_\6" [label="{{\6 | p\2}}", shape="record"];\
+    "e_\2_\4" [label="{{\4 | p\2}}", shape="record"];\
+    "e_\2_\4" -> "e_\2_\6" [label="exec", color="black"];/p'
+grep 'fork' $1 | sed -n -E 's/.*"event": "[[:alnum:]_-]*:[[:alnum:]_-]*:(.?fork[[:alnum:]_-]*):".*"pid": ([[:digit:]]*).*"uid": ([[:digit:]]*).*"exec": "([[:alnum:]_-]*)".*"new_pid": ([[:digit:]]*).*/    p_\2 [label="pid \2", shape="diamond"];\
+    "e_\5_\4" [label="{{\4 | p\5}}", shape="record"];\
+    "e_\2_\4" [label="{{\4 | p\2}}", shape="record"];\
+    "e_\2_\4" -> "e_\5_\4" [label="fork", color=gray15];/p'
+echo "}"
 } > ${file_name}_full.dot
 
 # make a png, removing orphaned nodes
 gvpr -c 'N[$.degree==0]{delete($G,$);}' ${file_name}_full.dot | dot -Tpng -o ${file_name}_full.dot.png
 
+{
+echo "strict digraph $file_name {";
+echo "rankdir=\"LR\";"
+echo "\"sample\" [label=\"root user\", shape=\"box\", style=\"filled\", fillcolor=\"0.0 0.4 1.0\"]"
+echo "\"sample2\" [label=\"(other users\nvary in color)\", shape=\"box\", style=\"filled\", fillcolor=\"0.1001 0.4 1.0\"]"
+echo "\"sample\" -> \"sample2\""
+grep 'new_exec' $1 | sed -n -E 's/.*"event": "[[:alnum:]_-]*:[[:alnum:]_-]*:(.?exec[[:alnum:]_-]*):".*"pid": ([[:digit:]]*).*"uid": ([[:digit:]]*).*"exec": "([[:alnum:]_-]*)".*"new_exec": "([ [:alnum:]_.-]*\/)*([ [:alnum:]_.-]*)".*/    p_\2 [label="pid \2", shape="diamond"];\
+    "e_\2_\6" [label="{{\6 | p\2}}", shape="record", style="filled", fillcolor="0.\3 0.4 1.0"];\
+    "e_\2_\4" [label="{{\4 | p\2}}", shape="record", style="filled", fillcolor="0.\3 0.4 1.0"];\
+    "e_\2_\4" -> "e_\2_\6" [label="exec", color="black"];/p'
+grep 'fork' $1 | sed -n -E 's/.*"event": "[[:alnum:]_-]*:[[:alnum:]_-]*:(.?fork[[:alnum:]_-]*):".*"pid": ([[:digit:]]*).*"uid": ([[:digit:]]*).*"exec": "([[:alnum:]_-]*)".*"new_pid": ([[:digit:]]*).*/    p_\2 [label="pid \2", shape="diamond"];\
+    "e_\5_\4" [label="{{\4 | p\5}}", shape="record", style="filled", fillcolor="0.\3 0.4 1.0"];\
+    "e_\2_\4" [label="{{\4 | p\2}}", shape="record", style="filled", fillcolor="0.\3 0.4 1.0"];\
+    "e_\2_\4" -> "e_\5_\4" [label="fork", color=gray15];/p'
+echo "}"
+} > ${file_name}_execs.dot
+
+# make a png, removing orphaned nodes
+gvpr -c 'N[$.degree==0]{delete($G,$);}' ${file_name}_execs.dot | dot -Tpng -o ${file_name}_execs.dot.png
