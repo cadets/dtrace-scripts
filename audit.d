56bc1703516cd46c6cacb3282ac262e2cbc0691c
Arun Thomas <arun.thomas@baesystems.com>
2016-07-18 00:53:56 -0230

    Support more DTAudit probes

--- audit.d
+++ audit.d
@@ -76,29 +76,11 @@ inline int af_inet6 = 28 /*AF_INET6*/;
 #define ARG_ALL            0xFFFFFFFFFFFFFFFFULL
 #define IS_VALID(arg)  (args[1]->ar_valid_arg & (arg))
 
-BEGIN {
-    printf("[\n");
-    comma=" ";
-}
-
-END {
-  printf("]\n");
-}
-
-/*
-audit::aue_*:commit
-/pid != $pid/
-{
-    this->record = (struct audit_record*) arg1;
-    printf("%s {\"event\": \"%s:%s:%s:\", \"valid_mask\": %x }\n",
-        comma, probeprov, probemod, probefunc, this->record->ar_valid_arg);
-}
-*/
-
 /* Default filter on processes */
-#define proc_filter_def pid != $pid
+#define proc_filter_def (pid != $pid)
 /* Filter on processes for read/write/mmap */
-#define proc_filter_rw pid != $pid && execname != "sshd" && execname != "tmux" && execname != "moused"
+#define proc_filter_rw (pid != $pid) && (execname != "sshd") && \
+	(execname != "tmux") && (execname != "moused")
 
 
 /* Fields that every event record will have */
@@ -112,13 +94,123 @@ audit::aue_*:commit
     printf("%s {\"event\": \"%s:%s:%s:\", \"time\": %d, \"pid\": %d, \"ppid\": %d, \"tid\": %d, \"uid\": %d, \"exec\": \"%s\"", comma, probeprov, probemod, probefunc, walltimestamp, pid, ppid, tid, uid, this->record->ar_subj_comm); \
     printf(", \"subjuuid\": \"%U\", \"procuuid\": \"%U\", \"obj1uuid\": \"%U\", \"obj2uuid\": \"%U\"", args[1]->ar_subj_uuid, args[1]->ar_arg_procuuid, args[1]->ar_arg_objuuid1, args[1]->ar_arg_objuuid2);
 
+#define sprint_audit_string(flag, field, name) \
+	IS_VALID(flag)?strjoin( strjoin(strjoin(", \"", #name), "\": \""), strjoin(stringof(this->record->field),"\"")):""
+#define sprint_audit_int(flag, field, name) \
+	IS_VALID(flag)?strjoin( strjoin(strjoin(", \"", #name), "\": "), lltostr(this->record->field)):""
+
+
+#define SINGLE_AUDIT_PROBE 1
+
+/*
+ * BEGIN and END probes
+ */
+BEGIN {
+    printf("[\n");
+    comma=" ";
+}
+
+END {
+  printf("]\n");
+}
+
+/*
+ * Use the single audit probe
+ */
+#if SINGLE_AUDIT_PROBE
+
+/* XXX: proc_filter */
+/* XXX: Conditionally print UUIDs */
+/* XXX: Enable only probes we want */
+audit::aue_*:commit
+/(pid != $pid) && ((execname != "sshd") || ((execname == "sshd") &&
+	(probefunc != "aue_read") && (probefunc != "aue_write") && (probefunc != "aue_mmap")))/
+{
+    this->record = (struct audit_record*) arg1;
+    print_common_fields;
+
+    printf("%s",
+	sprint_audit_int(ARG_PID, ar_arg_pid, arg_pid));
+    printf("%s",
+	sprint_audit_int(ARG_EUID, ar_arg_euid, arg_euid));
+    printf("%s",
+	sprint_audit_int(ARG_RUID, ar_arg_ruid, arg_ruid));
+    printf("%s",
+	sprint_audit_int(ARG_SUID, ar_arg_suid, arg_suid));
+    printf("%s",
+	sprint_audit_int(ARG_UID, ar_arg_uid, arg_uid));
+    printf("%s",
+	sprint_audit_int(ARG_EGID, ar_arg_egid, arg_egid));
+    printf("%s",
+	sprint_audit_int(ARG_RGID, ar_arg_rgid, arg_rgid));
+    printf("%s",
+	sprint_audit_int(ARG_SGID, ar_arg_sgid, arg_sgid));
+    printf("%s",
+	sprint_audit_int(ARG_GID, ar_arg_gid, arg_gid));
+    printf("%s",
+	sprint_audit_string(ARG_UPATH1, ar_arg_upath1, upath1));
+    printf("%s",
+	sprint_audit_string(ARG_UPATH2, ar_arg_upath2, upath2));
+    printf("%s",
+	sprint_audit_int(ARG_FFLAGS, ar_arg_fflags, flags));
+    printf("%s",
+	sprint_audit_int(ARG_FD, ar_arg_fd, fd));
+    printf("%s",
+	sprint_audit_int(ARG_ATFD1, ar_arg_atfd1, atfd1));
+    printf("%s",
+	sprint_audit_int(ARG_ATFD2, ar_arg_atfd2, atfd2));
+    printf("%s",
+	sprint_audit_int(ARG_MODE, ar_arg_mode, mode));
+    printf("%s",
+	sprint_audit_int(ARG_LEN, ar_arg_len, len));
+    printf("%s",
+	sprint_audit_int(ARG_SIGNUM, ar_arg_signum, signum));
+
+#if 0
+    printf("%s",
+	IS_VALID(ARG_SADDRINET)?
+	strjoin(", \"family\": ", lltostr(af_inet))
+	:IS_VALID(ARG_SADDRINET6)?
+	strjoin(", \"family\": ", lltostr(af_inet6))
+	:"");
+#endif
+
+    printf("%s",
+	IS_VALID(ARG_SADDRINET)?
+	strjoin(", \"address\": ", strjoin(
+	    inet_ntop(af_inet,(void*)&((struct sockaddr_in*) &this->record->ar_arg_sockaddr)->sin_addr), "\""))
+	:IS_VALID(ARG_SADDRINET6)?
+	strjoin(", \"address\": ", strjoin(
+	    inet_ntoa6(&((struct sockaddr_in6*) &this->record->ar_arg_sockaddr)->sin6_addr), "\""))
+	:IS_VALID(ARG_SADDRUNIX)?
+	strjoin(", \address\": ", strjoin(
+		((struct sockaddr_un*) &this->record->ar_arg_sockaddr)->sun_path, "\""))
+	:"");
+
+    printf("%s",
+	IS_VALID(ARG_SADDRINET)?
+	strjoin(", \"port\": ", lltostr(ntohs(((struct sockaddr_in*) &this->record->ar_arg_sockaddr)->sin_port)))
+        :IS_VALID(ARG_SADDRINET6)?
+	strjoin(", \"port\": ", lltostr(ntohs(((struct sockaddr_in6*) &this->record->ar_arg_sockaddr)->sin6_port)))
+	: "");
+
+    printf(", \"retval\": %d", this->record->ar_retval);
+    printf(", \"errno\": %d", this->record->ar_errno);
+
+    printf("}\n");
+    comma=",";
+}
+
+/* Individual audit probes */
+# else
+
 /*
  * Process probes
  */
 audit::aue_fork:commit,
 audit::aue_vfork:commit,
 audit::aue_rfork:commit
-/proc_filter_def && args[1]->ar_retval >= 0/
+/proc_filter_def && (args[1]->ar_retval >= 0)/
 {
     this->record = (struct audit_record*) arg1;
     print_common_fields;
@@ -152,7 +244,7 @@ audit::aue_exit:commit
  * Filesystem probes
  */
 audit::aue_open*:commit
-/proc_filter_def && args[1]->ar_retval >= 0/
+/proc_filter_def && (args[1]->ar_retval >= 0)/
 {
     this->record = (struct audit_record*) arg1;
     print_common_fields;
@@ -182,6 +274,42 @@ audit::aue_close:commit
     comma=",";
 }
 
+/* XXX: renameat */
+audit::aue_rename*:commit
+/proc_filter_def/
+{
+    this->record = (struct audit_record*) arg1;
+    print_common_fields;
+    printf(", \"path\": \"%s\", \"new_path\": \"%s\"}\n",
+        IS_VALID(ARG_UPATH1)?stringof(this->record->ar_arg_upath1):"",
+	IS_VALID(ARG_UPATH2)?stringof(this->record->ar_arg_upath2):"");
+    comma=",";
+}
+
+/* XXX: unlinkat */
+audit::aue_unlink*:commit
+/proc_filter_def/
+{
+    this->record = (struct audit_record*) arg1;
+    print_common_fields;
+    printf(", \"path\": \"%s\"}\n",
+        IS_VALID(ARG_UPATH1)?stringof(this->record->ar_arg_upath1):"");
+    comma=",";
+}
+
+/* XXX: ftruncate */
+audit::aue_truncate:commit,
+audit::aue_ftruncate:commit
+/proc_filter_def/
+{
+    this->record = (struct audit_record*) arg1;
+    print_common_fields;
+    printf(", \"path\": \"%s\", \"length\": %d}\n",
+        IS_VALID(ARG_UPATH1)?stringof(this->record->ar_arg_upath1):"",
+	IS_VALID(ARG_LEN)?this->record->ar_arg_len:0);
+    comma=",";
+}
+
 audit::aue_*read:commit,
 audit::aue_*readv:commit,
 audit::aue_*write:commit,
@@ -197,7 +325,7 @@ audit::aue_*writev:commit
 }
 
 audit::aue_mmap:commit
-/proc_filter_rw && IS_VALID(ARG_FD) && args[1]->ar_arg_fd != -1/
+/proc_filter_rw && IS_VALID(ARG_FD) && (args[1]->ar_arg_fd != -1)/
 {
     /*TODO missing path */
     this->record = (struct audit_record*) arg1;
@@ -233,3 +361,4 @@ audit::aue_accept*:commit
         this->sockaddr, this->sockport, errno);
     comma=",";
 }
+#endif
